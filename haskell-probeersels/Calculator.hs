6f2e19aff757b9226a019ad59d44be6da6fc12e4
delamiro <jamiro.aliet2@outlook.com>
2025-09-02 09:04:54 +0200

    added divide to calculator app
    added divide aswell

--- haskell-probeersels/Calculator.hs
+++ haskell-probeersels/Calculator.hs
@@ -1,24 +1,23 @@
 
 
-calc :: String -> Double -> Double -> Double
+calc :: String -> Float -> Float -> Float
 calc op a b
     | op == "+" = a + b
     | op == "-" = a - b
     | op == "*" = a * b
+    | op == "/" = a / b
+
 
 main :: IO ()
 main = do
     putStrLn "What type of calculation do you want to do?"
     calcStr <- getLine
     putStrLn "What is the first number?"
-    firstNumber <- readLn :: IO Double
+    firstNumber <- readLn :: IO Float
     putStrLn "What is the second number?"
-    secondNumber <- readLn :: IO Double
+    secondNumber <- readLn :: IO Float
 
     let result = calc calcStr firstNumber secondNumber
-
-
     
-
     -- "show" in this print makes the result a Str instead of a Int
     putStrLn ("Your number is: " ++ show result)
